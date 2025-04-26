35e4a88b995a6b4392b2f96c2f4c81455d5d2ab1
DevZone51 <103198509+DevZone51@users.noreply.github.com>
2025-04-26 20:48:42 +0100

    Update DML_INS_ITEM_LOC_SOH.sql
    previous code inserting duplicated records

--- DML_INS_ITEM_LOC_SOH.sql
+++ DML_INS_ITEM_LOC_SOH.sql
@@ -1,11 +1,16 @@
+-- insert / commit each 10K registers and for each loop iteration ensures that do not exists item,loc on item_loc_soh
 BEGIN
   FOR i IN 1..10 LOOP
     INSERT INTO item_loc_soh (item, loc, dept, unit_cost, stock_on_hand)
     SELECT item, loc, dept,
            (DBMS_RANDOM.value(5000, 50000)),
            ROUND(DBMS_RANDOM.value(1000, 100000))
-    FROM item, loc
-    WHERE ROWNUM <= 100000;  -- Inserir em lotes de 100.000 registros
+    FROM item im, loc loc
+    WHERE ROWNUM <= 10000
+      AND NOT EXISTS ( SELECT 1
+                         FROM ITEM_LOC_SOH ils
+                        WHERE ils.item = im.item
+                           AND ils.loc = loc.loc);  
     COMMIT;
   END LOOP;
 END;
